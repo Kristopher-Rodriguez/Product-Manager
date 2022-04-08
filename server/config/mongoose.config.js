02257370fca8bd48814a07d193fe39bbcf3399db
Kris Rodriguez <k.rod.518@gmail.com>
2022-04-07 20:41:52 -0500

    Changed Database to correct database name

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
+const productDb = "productDb";
+//productDb is the database name, it will be created if it does not exist.
+
 mongoose
-  .connect("mongodb://localhost/crmdb", {
+  .connect(`mongodb://localhost/${productDb}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Established a connection to the database"))
+  .then(() =>
+    console.log(`Established a connection to the database ${productDb}`)
+  )
   .catch((err) =>
     console.log("Something went wrong when connecting to the database", err)
   );
--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,17 +1,18 @@
 const Product = require("../models/product.model");
 
-module.exports.findAllProducts = (request, response) => {
+module.exports.findAllProducts = (req, res) => {
   Product.find()
-      .then((allProducts) => {
-          response.json({ product: allProducts })
-      })
-      .catch((err) => {
-          response.json({ message: 'Something went wrong', error: err })
-      });
-}
+    .then((allProducts) => {
+      console.log(allProducts);
+      res.json({ product: allProducts });
+    })
+    .catch((err) => {
+      res.json({ message: "Something went wrong", error: err });
+    });
+};
 
-module.exports.createProduct = (request, response) => {
-  Product.create(request.body)
-    .then((product) => response.json(product))
-    .catch((err) => response.json(err));
+module.exports.createProduct = (req, res) => {
+  Product.create(req.body)
+    .then((newProduct) => res.json(newProduct))
+    .catch((err) => res.json(err));
 };
